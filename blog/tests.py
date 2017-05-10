e5e3e287133da093718a38d0293b99ba2c6e432e
fumin <fumin@znzcloud.com>
2017-05-10 18:36:15 +0800

    blog test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from blog.models import Note
 
-# Create your tests here.
+
+class NoteTestCase(TestCase):
+    def setUp(self):
+        Note.objects.create(title='test django', body='the is test case')
+
+    def test_note_can_url(self):
+        title = Note.objects.get(title='test django')
+        time = title.publish
+        self.assertEqual(title.get_absolute_url(), u'/blog/detail/%s/%s/%s/%s/' %
+                         (time.year, time.strftime('%m'), time.strftime('%d'), title.id))
